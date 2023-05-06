9c9256e60f7613d9b6e233c1f8a0ed3b9053c5b1
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-05-06 08:02:39 -0300

    fix: helper icon alignment

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,16 +1,19 @@
 import React from "react"
 import  './styles.css'
 import { TooltipProps } from "./types";
-import { TooltipAnchor, Tooltip as TooltipComponent, useTooltipStore } from "@ariakit/react";
+import { TooltipAnchor, TooltipArrow, Tooltip as TooltipComponent, useTooltipStore } from "@ariakit/react";
 
 export default function Tooltip({ as, message, children, portal }: TooltipProps) {
   const tooltip = useTooltipStore();
   return(
     <>
       <TooltipAnchor className="uk-anchor" as={as} store={tooltip}>
-        {children}
+        <div style={{ display: 'flex'}}>
+         {children}
+        </div>
       </TooltipAnchor>
       <TooltipComponent className="uk-tooltip" portal={portal} store={tooltip}>
+        <TooltipArrow />
         {message}
       </TooltipComponent>
     </>
