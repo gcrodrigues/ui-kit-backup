404f7d208a761eccd382cb64479483ab25057e0d
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-05-06 08:01:37 -0300

    feat: add tag component

---
+++ src/components/Tag/types.ts
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from "react";
+
+export interface TagProps extends PropsWithChildren{
+  onClick: React.MouseEventHandler<HTMLButtonElement>
+  color?: string,
+  borderColor?: string
+  backgroundColor?: string
+  hasButton?: boolean 
+}
